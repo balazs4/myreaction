57d05f2487c02489c40742dc12d7d8053d518a04
balazs4 <balazs4web@gmail.com>
2016-11-06 21:41:08 +0100

    Add redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
-render(<App />, document.getElementById('app'));
+const store = createStore((state, action) => state)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
