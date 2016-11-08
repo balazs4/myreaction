c293642e8adbd83e0f3db304637fc13981d1a150
balazs4 <balazs4web@gmail.com>
2016-11-08 16:19:15 +0100

    basic redux implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.state = {
+            username: ''
+        }
+    }
+
+
+    handleInputChange(e) {
+        this.setState({ username: e.target.value });
+    }
+
     render() {
+        const {fetch, isFetching} = this.props;
+        const {username} = this.state;
         return (
             <div>
-                <h1>This is an webapp</h1>
+                <input type="text" placeholder="Username" value={username} onChange={this.handleInputChange} />
+                <button type="button" onClick={e => fetch(username)} disabled={isFetching}>Fetch</button>
+                {isFetching ? <p>Fetching...</p> : <div />}
             </div>
         );
     }
 }
 
-export default App;
+export default connect(
+    state => ({
+        isFetching: state.isFetching
+    }),
+    dispatch => ({
+        fetch: (username) => {
+            dispatch({ type: 'FETCH_USER', username })
+        }
+    })
+)(App);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,24 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 
-const store = createStore((state, action) => state)
+
+
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_USER':
+            return Object.assign({}, state, { isFetching: true })
+        case 'FETCH_USER_REJECT':
+        case 'FETCH_USER_RESOLVE':
+            return Object.assign({}, state, { isFetching: false });
+
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer, {})
 
 render(
     <Provider store={store}>
